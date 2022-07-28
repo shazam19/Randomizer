74fbd0541ec84f32d866069d8c4af40e9bda550a
szaman <shamsad.zaman@selise.ch>
2022-07-28 21:29:23 +0600

    add name card

---
+++ Blazor.Randomizer.WebAssembly/Pages/Randomizer/NameCard.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Randomizer.WebAssembly.Pages.Randomizer
+{
+    public partial class NameCard
+    {
+        [Parameter]
+        public string Name { get; set; } = "Umar bin Khattab";
+
+        [Parameter]
+        public string Description { get; set; } = "Khalifatun Rashedin";
+
+        [Parameter]
+        public string ImageUrl { get; set; } = @"https://cdn2.iconfinder.com/data/icons/avatar-vol-1-5/512/7_Asian_avatar_businessman_chinese-512.png";
+    }
+}
