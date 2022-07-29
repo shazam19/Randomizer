d401b3c47edaebc793ff6e38d44624d241c638a5
szaman <shamsad.zaman@selise.ch>
2022-07-29 16:49:53 +0600

    add delete functionality to name card; add css for advanced randomizer

--- Blazor.Randomizer.WebAssembly/Pages/Randomizer/NameCard.razor.cs
+++ Blazor.Randomizer.WebAssembly/Pages/Randomizer/NameCard.razor.cs
@@ -5,6 +5,9 @@ namespace Blazor.Randomizer.WebAssembly.Pages.Randomizer
     public partial class NameCard
     {
         [Parameter]
+        public Guid Id { get; set; }
+
+        [Parameter]
         public string Name { get; set; } = "Umar bin Khattab";
 
         [Parameter]
@@ -12,5 +15,13 @@ namespace Blazor.Randomizer.WebAssembly.Pages.Randomizer
 
         [Parameter]
         public string ImageUrl { get; set; } = @"https://cdn2.iconfinder.com/data/icons/avatar-vol-1-5/512/7_Asian_avatar_businessman_chinese-512.png";
+
+        [Parameter, EditorRequired]
+        public EventCallback OnDeleteButtonClick { get; set; }
+
+        private async Task OnDeleteClickInternal()
+        {
+            await OnDeleteButtonClick.InvokeAsync();
+        }
     }
 }
--- Randomizer.Model/Model/Randomizee.cs
+++ Randomizer.Model/Model/Randomizee.cs
@@ -14,5 +14,9 @@ namespace Randomizer.Model.Model
         public Guid Id { get; set; }
 
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
