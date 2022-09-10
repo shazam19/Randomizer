d99149e5ce11bc317cba5a8d1f5a9d1ab8ec127e
szaman <shamsad.zaman@selise.ch>
2022-09-11 00:25:16 +0600

    add core library for front end

---
+++ Sz.Fe.Core/Picker/ImagePicker.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sz.Fe.Core.Picker
+{
+    public partial class ImagePicker
+    {
+        [Parameter]
+        public IList<string> ImageUrls { get; set; }
+
+        [Parameter]
+        public string ImageUrl { get; set; }
+    }
+}
