cd01e0d5a58e685aa7c3efd09910a60ba8190c27
szaman <shamsadzaman@yahoo.com>
2022-09-11 16:23:36 +0600

    - add border to selected avater - add code to check life cycle

--- Sz.Fe.Core/Picker/ImagePicker.razor.cs
+++ Sz.Fe.Core/Picker/ImagePicker.razor.cs
@@ -9,19 +9,112 @@ namespace Sz.Fe.Core.Picker
 {
     public partial class ImagePicker
     {
+        private IList<string> _imageUrls;
+
         [Parameter]
-        public IList<string> ImageUrls { get; set; }
+        public IList<string> ImageUrls
+        {
+            get
+            {
+                return _imageUrls;
+            }
+            set
+            {
+                if(_imageUrls != null && _imageUrls.Equals(value))
+                {
+                    Console.WriteLine("same imageUrls; return");
+                    return;
+                }
+
+                _imageUrls = value;
+
+                //_imageDatas = new List<ImageData>();
+
+                //foreach(var url in value)
+                //{
+                //    _imageDatas.Add(new ImageData(url));
+                //}
+
+                Console.WriteLine("inside imageUrls set");
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if(_imageUrls == null)
+            {
+                Console.WriteLine($"imageUrl null on Initialize");
+                return;
+            }
+
+            Console.WriteLine("inside initialize");
+
+            _imageDatas = new List<ImageData>();
+
+            foreach (var url in _imageUrls)
+            {
+                _imageDatas.Add(new ImageData(url));
+            }
+        }
+
+        public override Task SetParametersAsync(ParameterView parameters)
+        {
+            Console.WriteLine("inside SetParameterAsync");
+            return base.SetParametersAsync(parameters);
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            Console.WriteLine("inside parameterSet");
+        }
 
         public string SelectedImageUrl { get; private set; }
 
         [Parameter]
         public EventCallback OnImageSelected { get; set; }
 
-        private async Task OnImageUrlClick(string imageUrl)
+        private IList<ImageData> _imageDatas;
+
+        private async Task OnImageUrlClick(ImageData imageData)
         {
-            SelectedImageUrl = imageUrl;
+            SelectedImageUrl = imageData.ImageUrl;
+
+            foreach(var data in _imageDatas)
+            {
+                data.IsSelected = false;
+            }
+
+            imageData.IsSelected = true;
+
+            //StateHasChanged();
 
             await OnImageSelected.InvokeAsync();
+
+            //imageData.IsSelected = false;
+
+
+            //foreach (var data in _imageDatas)
+            //{
+            //    Console.WriteLine($"url: {data.ImageUrl} isSelected: {data.IsSelected}");
+            //    //data.IsSelected = false;
+            //}
         }
+    }
+
+    internal class ImageData
+    {
+
+        public ImageData(string url)
+        {
+            ImageUrl = url;
+        }
+
+        public string ImageUrl { get; private set; }
+
+        public bool IsSelected { get; set; }
     }
 }
