d2ec58c11276070374be2e1b29874aaddaafb1eb
szaman <shamsadzaman@yahoo.com>
2022-12-11 23:33:05 +0600

    add ImageButton for re-usability

---
+++ Sz.Fe.Core/Button/ImageButton.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sz.Fe.Core.Button
+{
+    public partial class ImageButton
+    {
+        [Parameter]
+        public string? ImageSource { get; set; }
+
+        //[Parameter]
+        //public string? AltText { get; set; }
+
+        [Parameter]
+        public string ButtonText { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> CapturedAttributes { get; set; } = new Dictionary<string, object>
+        {
+            { "class", "btn btn-close" }
+        };
+
+        [Parameter]
+        [Required]
+        public EventCallback OnButtonClick { get; set; }
+
+        private async Task ButtonClicked()
+        {
+            //if(OnButtonClick is null)
+            //{
+            //    throw NullReferenceException($"{nameof(OnButtonClick)} cannot be null");
+            //}
+
+            await OnButtonClick.InvokeAsync();
+        }
+    }
+}
